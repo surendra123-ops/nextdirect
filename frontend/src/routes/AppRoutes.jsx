9ea69ac49ee9e5fad0b71de0330e30fe621de9fb
surendrakumarreddy79@gmail.com <surendrayenika@gmail.com>
2026-05-13 23:47:37 +0530

    home page protected route removed and dpeloyed also

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AppLayout } from "../layouts/AppLayout.jsx";
 import { ProtectedRoute } from "./ProtectedRoute.jsx";
-import { HomeGate } from "./HomeGate.jsx";
+import { LandingPage } from "../pages/LandingPage.jsx";
 import { LoginPage } from "../pages/LoginPage.jsx";
 import { RegisterPage } from "../pages/RegisterPage.jsx";
 import { ProfilePage } from "../pages/ProfilePage.jsx";
@@ -12,7 +12,7 @@ export function AppRoutes() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-        <Route path={ROUTES.home} element={<HomeGate />} />
+        <Route path={ROUTES.home} element={<LandingPage />} />
         <Route path={ROUTES.login} element={<LoginPage />} />
         <Route path={ROUTES.register} element={<RegisterPage />} />
 
